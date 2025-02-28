e34352f6dd352dab34e4cb36d349a467e8b6ffcc
Achyut <achyut313@gmail.com>
2025-02-28 14:02:25 +0545

    Added Stat

--- components/contest/ContestTabs.jsx
+++ components/contest/ContestTabs.jsx
@@ -1,46 +1,32 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
+import "swiper/css";
+import "swiper/css/free-mode";
 
 const ContestTabs = ({ active, gameId }) => {
   const tabList = [
-    {
-      title: "Contest",
-      id: 1,
-      url: `/${gameId}/contest`,
-    },
-    {
-      title: "My Picks",
-      id: 2,
-      url: `/${gameId}/my-picks`,
-    },
-    {
-      title: "Leaderboard",
-      id: 3,
-      url: `/${gameId}/leaderboard`,
-    },
-    {
-      title: "Rules",
-      id: 4,
-      url: `/${gameId}/rules`,
-    },
+    { title: "Contest", id: 1, url: `/${gameId}/contest` },
+    { title: "My Picks", id: 2, url: `/${gameId}/my-picks` },
+    { title: "Leaderboard", id: 3, url: `/${gameId}/leaderboard` },
+    { title: "Rules", id: 4, url: `/${gameId}/rules` },
+    { title: "Stats", id: 5, url: `/${gameId}/stats` }, // Fixed duplicate id
   ];
+
   const [activeTab, setActiveTab] = useState(active);
-  console.log(active);
+
   return (
     <div className="max-w-[430px] relative px-[10px] w-full mx-auto">
-      <div className="w-full flex justify-between p-3 rounded-full bg-white shadow-sm">
+      <div className="w-full flex p-3 gap-3 overflow-x-auto whitespace-nowrap scrollbar-hide rounded-full bg-white shadow-sm custom-scrollbar">
         {tabList.map((d) => {
-          const active = d.id === activeTab;
-
+          const isActive = d.id === activeTab;
           return (
             <Link
               key={d.id}
               className={`text-[14px] rounded-full px-[12px] py-[7px] font-bold text-white ${
-                active ? "bg-primary" : "bg-[#FFBEA9]"
+                isActive ? "bg-primary" : "bg-[#FFBEA9]"
               }`}
               href={d.url}
-              // onClick={() => setActiveTab(d.id)}
             >
               {d.title}
             </Link>
